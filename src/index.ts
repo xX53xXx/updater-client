2c4f813c7faf899bc9bf2ec4d51f15b7fda00905
xX53xXx <53x@acevik.de>
2023-01-31 17:03:08 +0100

    Fix

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import fs from 'fs/promises';
 import { execSync } from 'child_process';
 
-const data = execSync('ffmpeg -list_devices true -f dshow -i dummy').toString();
-fs.appendFile('C:\\bin\\updater-client\\test.txt', `${new Date()}:\r\n${data}\r\n\r\n`);
+let rsp = '';
+
+try {
+    rsp = execSync('ffmpeg -list_devices true -f dshow -i dummy', { encoding: 'utf-8', windowsHide: true });
+    if (typeof rsp === 'object') {
+        rsp = (rsp as Record<string, string>).stdout;
+    }
+} catch (err) {
+    rsp = err?.stderr;
+}
+
+fs.appendFile('C:\\bin\\updater-client\\test.txt', `${new Date()}:\r\n${rsp}\r\n\r\n`);
 
