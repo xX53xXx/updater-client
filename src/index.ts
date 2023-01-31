c04b125f3ab9a8904983e6a578234ced2653024f
xX53xXx <53x@acevik.de>
2023-01-31 16:48:32 +0100

    Test

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import fs from 'fs/promises';
+import { execSync } from 'child_process';
 
-fs.appendFile('./test.txt', `\r\n${new Date()}`);
+const data = execSync('ffmpeg -list_devices true -f dshow -i dummy').toString();
+fs.appendFile('C:\\bin\\updater-client\\test.txt', `${new Date()}:\r\n${data}\r\n\r\n`);
 
